b88e5de965ccb22b969b490169c6ce86cf9d1865
jyeooeoeon <48303257+jyeooeoeon@users.noreply.github.com>
2021-02-16 17:36:10 +0900

    TravelSite
    (개발중) 향 후, 여행사 상품 통합적으로 모아보기

---
+++ tour.js
@@ -0,0 +1,9 @@
+ // function night() {
+ //    if(document.querySelector('#night_day').value === 'night') {
+ //        document.querySelector('body').style.backgroundColor = 'black';
+ //        document.querySelector('#night_day').value = 'day';
+ //    } else {
+ //        document.querySelector('body').style.backgroundColor = 'white';
+ //        document.querySelector('#night_day').value = 'night';
+ //    }
+ // }
